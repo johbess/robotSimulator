acec88ab5cc5fd8747fbebe4867b93ada4d6f91a
Johan Besseling <johan_besseling@hotmail.com>
2024-09-03 10:58:25 +0200

    added some initial tests

--- src/test/java/TestRobotSimulator.java
+++ src/test/java/TestRobotSimulator.java
@@ -1,6 +1,9 @@
+import com.hiq.robotSimulator.Direction;
 import com.hiq.robotSimulator.Robot;
 import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
 
 public class TestRobotSimulator {
 
@@ -11,7 +14,79 @@ public class TestRobotSimulator {
     }
 
     @Test
-    public void whenIllegalInputIsGivenRobot_RobotIgnoresCommand() {
+    public void whenInputIsNull_RobotIgnoresCommand() {
+        sut.postCommand(null);
+
+        assertTrue(sut.getDirection() == null);
+        assertTrue(sut.getX() == 0);
+        assertTrue(sut.getY() == 0);
+    }
+
+    @Test
+    public void whenInputIsBlankString_RobotIgnoresCommand() {
+        sut.postCommand("             ");
+
+        assertTrue(sut.getDirection() == null);
+        assertTrue(sut.getX() == 0);
+        assertTrue(sut.getY() == 0);
+    }
+
+    @Test
+    public void whenInputIsInvalid_RobotIgnoresCommand() {
+        sut.postCommand("invalid input");
+
+        assertTrue(sut.getDirection() == null);
+        assertTrue(sut.getX() == 0);
+        assertTrue(sut.getY() == 0);
+    }
+
+    @Test
+    public void whenInputWillWalkRobotOutOfBounds_RobotStaysAtBoundary() {
+        sut.postCommand("PLACE 0,0,EAST");
+        sut.postCommand("MOVE");
+        sut.postCommand("MOVE");
+        sut.postCommand("MOVE");
+        sut.postCommand("MOVE");
+        sut.postCommand("MOVE");
+        sut.postCommand("MOVE");
+        sut.postCommand("MOVE");
+        sut.postCommand("MOVE");
+        sut.postCommand("MOVE");
+
+        assertTrue(sut.getDirection().equals(Direction.EAST));
+        assertTrue(sut.getX() == 4);
+        assertTrue(sut.getY() == 0);
+    }
+
+    @Test
+    public void whenInputWillWalkRobotAnEntireLap_RobotEndsUpAtStart() {
+        sut.postCommand("PLACE 0,0,NORTH");
+        sut.postCommand("MOVE");
+        sut.postCommand("MOVE");
+        sut.postCommand("MOVE");
+        sut.postCommand("MOVE");
+
+        sut.postCommand("RIGHT");
+        sut.postCommand("MOVE");
+        sut.postCommand("MOVE");
+        sut.postCommand("MOVE");
+        sut.postCommand("MOVE");
+
+        sut.postCommand("RIGHT");
+        sut.postCommand("MOVE");
+        sut.postCommand("MOVE");
+        sut.postCommand("MOVE");
+        sut.postCommand("MOVE");
+
+        sut.postCommand("RIGHT");
+        sut.postCommand("MOVE");
+        sut.postCommand("MOVE");
+        sut.postCommand("MOVE");
+        sut.postCommand("MOVE");
+
+        assertTrue(sut.getDirection().equals(Direction.WEST));
+        assertTrue(sut.getX() == 0);
+        assertTrue(sut.getY() == 0);
 
     }
 }
